0227c2a17afd073a0c9a09902c2ea00edf4dff27
ashfaq-ui <rizvimahamed456@gmail.com>
2025-08-26 21:29:11 +0530

    backend update

--- frontend/src/app/User_profile/page.js
+++ frontend/src/app/User_profile/page.js
@@ -1,77 +1,116 @@
 "use client";
-import styles from "./user.module.css"
+import styles from "./user.module.css";
 import { useState } from "react";
+import axios from "axios";
 
+export default function UserProfile() {
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: "",
+    dob: "2000-01-01",
+  });
 
-export default function UserProfile(){
-        const [dob, setDob] = useState("2000-01-01");
+  const [message, setMessage] = useState("");
 
-// reset state to initial value.  leran how to reset the date ???
+  // Handle input change
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
+  // Handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://127.0.0.1:5000/api/users", formData);
+      setMessage(res.data.message);
 
+      // Reset the form after submission
+      setFormData({
+        firstname: "",
+        lastname: "",
+        dob: "2000-01-01",
+      });
+    } catch (err) {
+      console.error("Error submitting form:", err);
+      setMessage("Failed to submit!");
+    }
+  };
 
-    return(
-            <div className={styles["background-colour"]} >
-                <div className={styles["sub-whole-area"]} >
-                    <h1>hello world!</h1>
-                </div>
-{/* 
-                action="mailto:yourmail@example.com" method="POST" enctype="text/plain"
-                in here leran why we use method and action and enctype?
-                */}
+  return (
+    <div className={styles["background-colour"]}>
+      <div className={styles["sub-whole-area"]}>
+        <h1>User Profile Form</h1>
+      </div>
 
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>User Details</legend>
+          <br />
+          <label htmlFor="firstname">First Name:</label>
+          <input
+            type="text"
+            id="firstname"
+            name="firstname"
+            size="30"
+            placeholder="Enter your First name"
+            value={formData.firstname}
+            onChange={handleChange}
+            required
+          />
+          <br />
+          <br />
+          <label htmlFor="lastname">Last Name:</label>
+          <input
+            type="text"
+            id="lastname"
+            name="lastname"
+            size="30"
+            placeholder="Enter your Last name"
+            value={formData.lastname}
+            onChange={handleChange}
+            required
+          />
+        </fieldset>
 
-                <form >
-                    <fieldset>
-                        <legend>User Details</legend>
-                            <br />
-                            <label htmlFor="firstname">First Name :</label>
-                            <input 
-                                type="text" 
-                                id="firstname" 
-                                name="name" 
-                                size="30" 
-                                placeholder="Enter your First name" 
-                                required/>
+        <br />
 
-                            <br />
-                            <br />
-                            <label htmlFor="lastname">Last Name :</label>
-                            <input 
-                                type="text" 
-                                id="lastname" 
-                                name="name" 
-                                size="30" 
-                                placeholder="Enter your Last name" 
-                                required/>
-                    </fieldset>
-                        <br />
-                    <fieldset>
-                        <legend>Dob</legend>
+        <fieldset>
+          <legend>Date of Birth</legend>
+          <br />
+          <label htmlFor="dob">Date Of Birth:</label>
+          <input
+            type="date"
+            id="dob"
+            name="dob"
+            value={formData.dob}
+            onChange={handleChange}
+            required
+          />
+        </fieldset>
 
-                            <br />
-                            <label htmlFor="dob">Date Of Birth</label>
-                            <input 
-                                type="date" 
-                                id="dob" 
-                                name="dob" 
-                                value={dob}
-                                onChange={(e) => setDob(e.target.value)}
-                                required />
-                    </fieldset>
-                    <br />
-                    <fieldset>
-                        <br />
-                        <input type="reset" name="reset" value="Reset" />
-                        <br />
-                        <br />
-                        <input type="submit" name="submit" value="Sent" />
-                    </fieldset>
-                </form>
-                <div className={styles["whole-area"]} >
-                    <h1>hello world!</h1>
-                </div>
-            
-            </div>
-    );
+        <br />
+
+        <fieldset>
+          <br />
+          <button
+            type="button"
+            onClick={() =>
+              setFormData({
+                firstname: "",
+                lastname: "",
+                dob: "2000-01-01",
+              })
+            }
+          >
+            Reset
+          </button>
+          <br />
+          <br />
+          <button type="submit">Send</button>
+        </fieldset>
+      </form>
+
+      {message && <p style={{ marginTop: "10px", color: "green" }}>{message}</p>}
+    </div>
+  );
 }
